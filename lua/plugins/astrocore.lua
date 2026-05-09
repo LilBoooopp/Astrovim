339e381be9bf1d5232ca87ae4ed9cc619261537c
LilBoooopp <cbopp@student.42lausanne.ch>
2026-05-10 00:11:02 +0200

    hex dump

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -75,6 +75,8 @@ return {
           desc = "Close buffer from tabline",
         },
 
+        ["<Leader>xh"] = { "<cmd>HexToggle<cr>", desc = "Toggle hex view" },
+
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
